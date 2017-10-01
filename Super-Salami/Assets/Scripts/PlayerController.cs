aa1dbdd6c14de5d19a0bdf2dfb4134094ca62ea8
dustyamp <amprulesgreatly@gmail.com>
2017-10-01 11:40:25 -0700

    Full level with models
    Full level with models

---
+++ Super-Salami/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	public float gravity;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+	
+		Vector3 movement = new Vector3 (-moveHorizontal, gravity, gravity);
+		rb.AddForce (movement * speed);
+	}
+}
---
+++ Super-Salami/Assets/Scripts/RotatorX.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorX : MonoBehaviour {
+	void Update () {
+		transform.Rotate(new Vector3(45, 0, 0) * Time.deltaTime);
+	}
+}
