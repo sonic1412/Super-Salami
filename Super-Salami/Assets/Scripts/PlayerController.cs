4ad9fcad04a01e084d039b4cf48ee5c9615088e9
sonic1412 <sailixmonrow@gmail.com>
2017-10-01 11:47:03 -0700

    merge

--- Super-Salami/Assets/Scripts/PlayerController.cs
+++ Super-Salami/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,28 @@
+<<<<<<< HEAD
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	public float gravity;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+	
+		Vector3 movement = new Vector3 (-moveHorizontal, gravity, gravity);
+		rb.AddForce (movement * speed);
+	}
+}
+=======
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
@@ -49,3 +74,4 @@ public class PlayerController : MonoBehaviour {
 	}
 }
 
+>>>>>>> f42792e0ec53eaaf83f536bbf9ab0fee4b034c7e
